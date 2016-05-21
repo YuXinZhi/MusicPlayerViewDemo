9fa47049da412098de7b35bd442d0886e6ab2440
Ben <whoyumengmeng@gmail.com>
2016-05-21 14:05:24 +0800

    init

---
+++ musicapplication/src/main/java/com/example/musicapplication/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.musicapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 欢迎页面
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
